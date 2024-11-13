18b7ffbb7d13b85234af76ff2f18eecd79de0305
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-11-13 04:24:00 -0500

    Test

--- EC2.tf
+++ EC2.tf
@@ -753,6 +753,14 @@ resource "aws_launch_template" "my_launch_template" {
     name = aws_iam_instance_profile.ecs_instance_profile.name
   }
 
+
+    user_data = base64encode(<<-EOF
+              #!/bin/bash
+              echo "ECS_CLUSTER=${aws_ecs_cluster.ecs_cluster.name}" >> /etc/ecs/ecs.config
+              systemctl enable --now ecs
+              EOF
+  )
+  
   tag_specifications {
     resource_type = "instance"
 
